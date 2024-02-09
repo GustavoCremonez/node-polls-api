c2d249d6148b50a0f4bf7c4829fd5e23dd76ffeb
GustavoCremonez <aparecido2332001@gmail.com>
2024-02-09 20:04:25 -0300

    feat: Adiciona arquivo para levantar imagens do docker, conexão do prisma + postgres e criação de rota para adicionar enquente

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,24 @@
 import fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
+import { z } from "zod";
 
 const app = fastify();
+const prisma = new PrismaClient();
 
-app.get("/", () => {
-  return "Hello World";
+app.post("/polls", async (request, reply) => {
+  const createPollBody = z.object({
+    title: z.string(),
+  });
+
+  const { title } = createPollBody.parse(request.body);
+
+  const poll = await prisma.poll.create({
+    data: {
+      title,
+    },
+  });
+
+  return reply.status(201).send({ pollId: poll.id });
 });
 
 app.listen({ port: 3333 }).then(() => {
